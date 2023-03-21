874970a25fe42e2f790db2c8243b5e535060aae6
alexgaard <thomasjensen9258@gmail.com>
2023-03-21 16:59:19 +0100

    Added bibtex to build and tested it

--- report/compile.sh
+++ report/compile.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-pdflatex main.tex && makeglossaries main && pdflatex main.tex
+biber main && \
+    #pdflatex main.tex && \
+    biber main && \
+    makeglossaries main && \
+    pdflatex main.tex
