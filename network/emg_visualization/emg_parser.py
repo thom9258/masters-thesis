4087d5e0202e607349ad0da89d333e4ec6c2e54b
alexgaard <thomasjensen9258@gmail.com>
2023-05-03 16:38:51 +0200

    created filter comparisons

--- network/emg_visualization/emg_parser.py
+++ network/emg_visualization/emg_parser.py
@@ -1,5 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
+import itertools
+from itertools import product
 
 from scipy import signal 
 
@@ -72,25 +74,42 @@ class emgParser():
         print("... Parsed sEMG data!")
 #class emgParser
 
-def quickPlot(data, data_labels, axis_labels=["x", "y"]):
+def quickPlot(data, data_labels, axis_labels=["x", "y"], title=None):
     plt.figure()
     plt.xlabel(axis_labels[0])
     plt.ylabel(axis_labels[1])
     for i in range(0, len(data)):
         plt.plot(data[i], label=data_labels[i])
+    if title is not None:
+        plt.title(title)
     plt.legend()
     plt.grid()
     plt.show()
 
 
-def Buttersworth_Low(data, sampling_frequency, cutoff_frequency=10, order=5):
+def uniquecombinations(arr1, arr2):
+    res = []
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            res.append((arr1[i], arr2[j]))
+    return res
+
+
+def Notch(data, fs, fc, quality):
+    # Nyquist Frequency Normalization
+    w = fc / (fs / 2) 
+    b, a = signal.iirnotch(w, quality, fs)
+    return signal.filtfilt(b, a, data)
+
+
+def Buttersworth_Low(data, sampling_frequency, cutoff_frequency, order):
     # Nyquist Frequency Normalization
     w = cutoff_frequency / (sampling_frequency / 2) 
     b, a = signal.butter(order, w, btype='low')
     return signal.filtfilt(b, a, data)
 
 
-def Buttersworth_Band(data, lowcut, highcut, sampling_frequency, order=5):
+def Buttersworth_Band(data, lowcut, highcut, sampling_frequency, order):
     # Nyquist Frequency Normalization
     wl = lowcut / (sampling_frequency / 2)
     wh = highcut / (sampling_frequency / 2)
@@ -98,25 +117,58 @@ def Buttersworth_Band(data, lowcut, highcut, sampling_frequency, order=5):
     return signal.filtfilt(b, a, data)
 
 
+def Buttersworth_lowpass_test(data, cutoffs, orders, title=None):
+    fs = 200
+    f_graphs = [data]
+    f_labels = ["Raw"]
+    combinations = uniquecombinations(cutoffs, orders)
+    for pair in combinations:
+        c = pair[0]
+        o = pair[1]
+        f_graphs.append(Buttersworth_Low(data, fs, c, o))
+        f_labels.append(f"Buttersworth Lowpass (fc={c},order={o})")
+    quickPlot(f_graphs, f_labels, axis_labels=["Time", "mV"], title=title)
+    return f_graphs, f_labels
+
+def Buttersworth_bandpass_test(data, lowhighs, orders, title=None):
+    fs = 200
+    f_graphs = [data]
+    f_labels = ["Raw"]
+    combinations = uniquecombinations(lowhighs, orders)
+    for pair in combinations:
+        lh = pair[0]
+        o = pair[1]
+        f_graphs.append(Buttersworth_Band(data, lh[0], lh[1], fs, o))
+        f_labels.append(f"Buttersworth Bandpass (lowhigh={lh},order={o})")
+    quickPlot(f_graphs, f_labels, axis_labels=["Time", "mV"], title=title)
+    return f_graphs, f_labels
+
 if __name__ == "__main__":
     print("TEST:")
     emgcsvfile = "../datasets/grasp_dataset/tjens18_index_30s_2_Trigno_2801.csv"
     parser = emgParser(emgcsvfile)
-    tricep = parser.tricep[0::10]
+    emg = parser.arm1[0::10]
+    emg = emg[:300]
 
-    tricep_bwl = Buttersworth_Low(tricep,
-                                  sampling_frequency=200,
-                                  cutoff_frequency=10,
-                                  order=5)
+    # Buttersworth tests
+    if 1:
+        Buttersworth_lowpass_test(emg,
+                                  cutoffs=[10, 20, 30, 40, 50],
+                                  orders=[10],
+                                  title="Lowpass Cutoff Test")
 
-    tricep_bwb = Buttersworth_Band(tricep,
-                                   sampling_frequency=200,
-                                   lowcut=10,
-                                   highcut=2000,
-                                   order=5)
-    
-    quickPlot([tricep, tricep_bwl, tricep_bwb],
-              ["tricep", "tricep Buttersworth Lowpass",  "tricep Buttersworth Bandpass"],
-              axis_labels=["Time", "mV"])
+        Buttersworth_lowpass_test(emg,
+                                  cutoffs=[20],
+                                  orders=[2, 5, 8, 10],
+                                  title="Lowpass Order Test")
+
+        Buttersworth_bandpass_test(emg,
+                                lowhighs=[[10, 2000], [20, 2500], [50, 400]],
+                                   orders=[5],
+                                   title="Bandpass, Cutoff Test")
+
+    # notch = Notch(tricep, fs=200, fc=100, quality=1)
+    # quickPlot([tricep, notch], ["tricep", "notch"])
+
 
 
